793a0de29928c18c0d6c15b6f567c4cf3627b25b
RaQ13 <lukasz46@op.pl>
2021-08-02 17:49:53 +0200

    created repositorium, project configuration and structure

---
+++ portfolio-app/src/App.js
@@ -0,0 +1,11 @@
+import './scss/main.scss';
+import * as Scroll from 'react-scroll';
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+
+function App() {
+  return (
+      <h1>Working</h1>
+  );
+}
+
+export default App;
