da0bb4f024cfeb24390804489b3c5e5df26ea3c5
RaQ13 <lukasz46@op.pl>
2021-08-02 21:28:32 +0200

    added header

--- portfolio-app/src/components/HomeThreeColumns.js
+++ portfolio-app/src/components/HomeThreeColumns.js
@@ -10,15 +10,15 @@ export const HomeThreeColumns = () => {
                         Aliquam erat volutpat.</p>
                 </div>
                 <div className="column">
-                    <h2>10</h2>
-                    <span>oddanych worków</span>
+                    <h2>5</h2>
+                    <span>wspartych organizacji</span>
                     <p>Lorem ipsum dolor sit amet,
                         consectetur adipisc Pellentesque vel enim a elit viverra elementuma.
                         Aliquam erat volutpat.</p>
                 </div>
                 <div className="column">
-                    <h2>10</h2>
-                    <span>oddanych worków</span>
+                    <h2>7</h2>
+                    <span>zorganizowanych zbiórek</span>
                     <p>Lorem ipsum dolor sit amet,
                         consectetur adipisc Pellentesque vel enim a elit viverra elementuma.
                         Aliquam erat volutpat.</p>
